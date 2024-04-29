11904ee94b75af14696700dbd11c0778b02d60fc
andrew-hayworth22 <andy@andys-MacBook-Pro.local>
2024-04-29 18:45:39 -0400

    Added game fields

--- database/migrations/2024_04_07_015516_create_games_table.php
+++ database/migrations/2024_04_07_015516_create_games_table.php
@@ -17,12 +17,77 @@ return new class extends Migration
             $table->id();
             $table->foreignIdFor(Season::class)->constrained()->cascadeOnDelete();
             $table->foreignIdFor(Team::class, 'opponent_team_id')->constrained('teams');
-            $table->enum('location', ['home', 'away', 'neutral']);
+            $table->enum('location', ['Home', 'Away', 'Neutral']);
+            $table->enum('type', ['Regular Season', 'Conference Championship', 'Bowl Game', 'National Octafinals', 'National Quarterfinals', 'National Semifinals', 'National Championship']);
             $table->dateTime('date');
 
-            $table->integer('our_score');
+            // Our stats
+            $table->integer('our_score_q1');
+            $table->integer('our_score_q2');
+            $table->integer('our_score_q3');
+            $table->integer('our_score_q4');
 
-            $table->integer('opponent_score');
+            $table->integer('our_first_downs');
+
+            $table->integer('our_third_down_att');
+            $table->integer('our_third_down_conv');
+            $table->integer('our_fourth_down_att');
+            $table->integer('our_fourth_down_conv');
+            $table->integer('our_two_point_att');
+            $table->integer('our_two_point_conv');
+            $table->integer('our_red_zone_att');
+            $table->integer('our_red_zone_fg');
+            $table->integer('our_red_zone_td');
+
+            $table->integer('our_fumbles_lost');
+
+            $table->integer('our_penalties');
+            $table->integer('our_penalty_yds');
+
+            $table->integer('our_top_min');
+            $table->integer('our_top_sec');
+
+            // Opponent stats
+            $table->integer('opponent_score_q1');
+            $table->integer('opponent_score_q2');
+            $table->integer('opponent_score_q3');
+            $table->integer('opponent_score_q4');
+
+            $table->integer('opponent_first_downs');
+
+            $table->integer('opponent_rush_att');
+            $table->integer('opponent_rush_yds');
+            $table->integer('opponent_rush_tds');
+
+            $table->integer('opponent_pass_comp');
+            $table->integer('opponent_pass_att');
+            $table->integer('opponent_pass_yds');
+            $table->integer('opponent_pass_tds');
+
+            $table->integer('opponent_third_down_att');
+            $table->integer('opponent_third_down_conv');
+            $table->integer('opponent_fourth_down_att');
+            $table->integer('opponent_fourth_down_conv');
+            $table->integer('opponent_two_point_att');
+            $table->integer('opponent_two_point_conv');
+            $table->integer('opponent_red_zone_att');
+            $table->integer('opponent_red_zone_fg');
+            $table->integer('opponent_red_zone_td');
+
+            $table->integer('opponent_fumbles_lost');
+            $table->integer('opponent_ints');
+
+            $table->integer('opponent_punt_return_yds');
+            $table->integer('opponent_kick_return_yds');
+
+            $table->integer('opponent_punts');
+            $table->integer('opponent_punt_avg');
+
+            $table->integer('opponent_penalties');
+            $table->integer('opponent_penalty_yds');
+
+            $table->integer('opponent_top_min');
+            $table->integer('opponent_top_sec');
 
             $table->timestamps();
         });
